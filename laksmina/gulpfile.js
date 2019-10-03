e99af51cb1d26f59b89d97cd8cdaee49ad59afb2
Robertas Alseika <robertasalseika@Robertass-MBP.lan>
2019-10-03 13:42:19 +0300

    No icons,menu change new page

--- laksmina/gulpfile.js
+++ laksmina/gulpfile.js
@@ -1,32 +1,3 @@
-// var gulp = require('gulp'),
-//     connect = require('gulp-connect'),
-//     watch = require('gulp-watch'),
-//     sass = require('gulp-sass');
-
-// gulp.task('webserver', function () {
-//     connect.server({
-//         livereload: true,
-//         root: 'src'
-//     });
-// });
-
-// gulp.task('livereload', function () {
-//     gulp.src('.src/**/*')
-//         .pipe(connect.reload());
-// });
-
-// gulp.task('sass', function () {
-//     return gulp.src('./src/assets/styles/*.scss')
-//         .pipe(sass({ errLogToConsole: true }))
-//         .pipe(gulp.dest('./src/assets/styles'));
-//   });
-
-// gulp.task('watch', function () {
-//     gulp.watch('./src/assets/styles/*.scss', ['sass']);
-//     gulp.watch('./src/*.html' ['livereload']);
-// });
-
-// gulp.task('default', ['sass', 'webserver', 'watch', 'livereload']);
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
